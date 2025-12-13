27608bcbd0d6eb20ab1d0a51bed0497b6eb51384
Rasul Berdiniyazov <97217239+Developer-k-r@users.noreply.github.com>
2025-12-13 21:39:39 +0500

    ss

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8309145510:AAEqxyuJZtbR_O3CQQYpF3UVmh-ENQf5CHg"  
-ADMIN_ID = 5411260603  
+BOT_TOKEN = ""  
+ADMIN_ID =   
 DB_NAME = "bot_database.db"  
 HELP_LINK = "https://t.me/NStaksi/30"  
